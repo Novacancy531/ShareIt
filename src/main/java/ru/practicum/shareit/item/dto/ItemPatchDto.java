c69bcfb46ea409114984e89d2a78f0ffcc79f8dd
Александр Викторов <Novacancy531@yandex.ru>
2025-08-25 10:58:26 +0300

    Разделение монолита.

--- src/main/java/ru/practicum/shareit/item/dto/ItemPatchDto.java
+++
@@ -1,4 +0,0 @@
-package ru.practicum.shareit.item.dto;
-
-public record ItemPatchDto(Long id, String name, String description, Boolean available) {
-}
--- src/main/java/ru/practicum/shareit/item/dto/ItemRequestDto.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.item.dto;
-
-import lombok.Builder;
-
-@Builder
-public record ItemRequestDto(Long itemId, String name, Long ownerId) {
-}
-
